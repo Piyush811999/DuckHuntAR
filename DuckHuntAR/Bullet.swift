bfb828e4463b4f9a295572439f49bd91cbdcd6b9
Piyush <infopiyushmakwana@gmail.com>
2019-03-30 06:53:52 +0530

    added some assets and bullet object

---
+++ DuckHuntAR/Bullet.swift
@@ -0,0 +1,35 @@
+//
+//  Bullet.swift
+//  DuckHuntAR
+//
+//  Created by Piyush Makwana on 30/03/19.
+//  Copyright © 2019 Piyush Makwana. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+// Spheres that are shot at the "ships"
+class Bullet: SCNNode {
+    override init () {
+        super.init()
+        let sphere = SCNSphere(radius: 0.025)
+        self.geometry = sphere
+        let shape = SCNPhysicsShape(geometry: sphere, options: nil)
+        self.physicsBody = SCNPhysicsBody(type: .dynamic, shape: shape)
+        self.physicsBody?.isAffectedByGravity = false
+        
+        // see http://texnotes.me/post/5/ for details on collisions and bit masks
+//        self.physicsBody?.categoryBitMask = CollisionCategory.bullets.rawValue
+//        self.physicsBody?.contactTestBitMask = CollisionCategory.ship.rawValue
+        
+        // add texture
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named: "bullet_texture")
+        self.geometry?.materials  = [material]
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- DuckHuntAR/ViewController.swift
+++ DuckHuntAR/ViewController.swift
@@ -24,10 +24,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
         // Set the scene to the view
-        sceneView.scene = scene
+//        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -71,5 +71,52 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
+    }
+    
+ 
+    @IBAction func tapScreen(_ sender: UITapGestureRecognizer) {
+        let bulletsNode = Bullet()
+        
+        let (direction, position) = self.getUserVector()
+        bulletsNode.position = position // SceneKit/AR coordinates are in meters
+       
+        let bulletDirection  = SCNVector3(direction.x*4,direction.y*4,direction.z*4)
+        bulletsNode.physicsBody?.applyForce(bulletDirection, asImpulse: true)
+        sceneView.scene.rootNode.addChildNode(bulletsNode)
+    }
+    
+    func createBullet()->SCNNode{
+        let node = Bullet()
+        return node
+    }
+    
+    
+    // fire bullet in direction camera is facing
+    func fireBullet(){
+        var node = SCNNode()
+        node = createBullet()
+        let (direction, position) = self.getUserVector()
+        node.position = position
+        var nodeDirection = SCNVector3()
+
+            nodeDirection  = SCNVector3(direction.x*4,direction.y*4,direction.z*4)
+            node.physicsBody?.applyForce(nodeDirection, at: SCNVector3(0.5,0,0), asImpulse: true)
+        
+        node.physicsBody?.applyForce(nodeDirection , asImpulse: true)
+        sceneView.scene.rootNode.addChildNode(node)
+    }
+
+    
+    func getUserVector() -> (SCNVector3, SCNVector3) { // (direction, position)
+        if let frame = self.sceneView.session.currentFrame {
+            // 4x4  transform matrix describing camera in world space
+            let mat = SCNMatrix4(frame.camera.transform)
+            // orientation of camera in world space
+            let dir = SCNVector3(-1 * mat.m31, -1 * mat.m32, -1 * mat.m33)
+            // location of camera in world space
+            let pos = SCNVector3(mat.m41, mat.m42, mat.m43)
+            return (dir, pos)
+        }
+        return (SCNVector3(0, 0, -1), SCNVector3(0, 0, -0.2))
     }
 }
